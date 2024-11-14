949734f6d9274697fa4370a8e42e1051fbc0657e
unknown <shekinadhanunjay@gmail.com>
2024-11-14 11:48:07 +0530

    string methods part-2 in js

---
+++ string method-2.js
@@ -0,0 +1,43 @@
+
+// let str = "Hello World!";
+// Relace the substring “World!” with “My Dear Friend” ..
+// Write the program for that
+
+
+let str= "Hello World!";
+console.log(str.replace("World","My Dear Friend"));
+
+// let a=”Hungry”;
+// Repeat the “Hungry” for 10 times
+// Write the program for that
+
+
+let a="Hungry ";
+console.log(a.repeat(10));
+
+
+// let sentence = "The quick brown fox jumps over the lazy dog. The fox is clever.";
+// Find the last index of the string “fox”
+
+
+let sentence = "The quick brown fox jumps over the lazy dog. The fox is clever.";
+console.log(sentence.lastIndexOf("fox"))
+
+// 4.let message = "Hello, world!";
+// Find the string starts with “hi”
+// Write code for that
+
+
+let message = "Hello, world!";
+console.log(message.startsWith("hi"))
+
+
+// 5.let fileName = "document.pdf";
+// Find the string ends with “.pdf”
+// Write code for that
+
+
+let fileName = "document.pdf";
+let b=fileName.endsWith(".pdf")
+console.log(b)
+
